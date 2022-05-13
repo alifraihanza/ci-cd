839d7cbf695e4180babdffd4b81c3dad3f792ad8
Alif Raihanza Fauzan <alifraihanzaa@gmail.com>
2022-05-13 04:28:40 -0700

    bismillah

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const app = require("./app")
 const PORT = 5000
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Berjalan di port ${PORT}`)
+app.listen(process.env.PORT, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 })
+
+
+  
+// app.listen(process.env.PORT, () => {
+//     console.log(`Berjalan di port ${PORT}`)
+// })
